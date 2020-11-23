a59b57f8015fe8d86464075859815d1e4d6954bd
JorgeACardona <andres20cardona@gmail.com>
2020-11-23 11:27:27 -0500

    db conectada

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const docentes = require('./routes/docentes');
 const calificaciones = require('./routes/calificaciones');
 const inquietud = require('./routes/inquietud');
 const trabajo = require('./routes/trabajo');
-const cors = require("cors");
+
 
 // Ajustes
-app.set('port', process.env.PORT || 3001);
+app.set('port', 3000);
 
 // Middlewares
 app.use(express.json());
 
-app.use(cors({origin:'*'}));
+
 
 // Routes//
 app.use('/api',estudiante);
@@ -25,6 +25,6 @@ app.use('/api',inquietud);
 app.use('/api',trabajo);
 
 // Ajustes del servidor
-app.listen(3001, () => {
-  console.log(`running server port 3001`);
+app.listen(3000, () => {
+  console.log(`running server port 3000`);
 });
