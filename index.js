b28d86aaef45c2c75244b5b34b87d7b50c829ae4
JorgeACardona <andres20cardona@gmail.com>
2020-11-23 16:25:48 -0500

    deploy ceducational

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const materia = require('./routes/materia');
 const docentes = require('./routes/docentes');
 const calificaciones = require('./routes/calificaciones');
 const inquietud = require('./routes/inquietud');
-const trabajo = require('./routes/trabajo');
+
 const cors = require('cors')
 const bodyParser = require('body-parser');
 
@@ -31,7 +31,7 @@ app.use('/api',docentes);
 app.use('/api',materia);
 app.use('/api',calificaciones);
 app.use('/api',inquietud);
-app.use('/api',trabajo);
+
 
 // Ajustes del servidor
 app.listen(app.get('port'), () => {
