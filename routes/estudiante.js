398fc2ce2e9b5ba285ccab778e6b6a9c9aa28153
JorgeACardona <andres20cardona@gmail.com>
2020-11-24 17:05:59 -0500

    intento 4

--- routes/estudiante.js
+++ routes/estudiante.js
@@ -1,48 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-const mysqlConnection  = require('../db/db');
+const mysqlConnection = require('../db/db');
 
 router.get('/estudiantes', (req, res) => {
-     
+
   mysqlConnection.query('SELECT * FROM estudiante ', (err, rows, fields) => {
-      if (!err) {
-        res.json(rows);
-      } else {
-        console.log(err);
-      }
-    });
-  });
-
-router.post('/nuevoEstudiante/',(req,res)=>{
-
-const {nombre,apellido,grado,acudiente,correo,contraseña,cod_trabajo,fecha_nacimiento,id_estudiante,edad} = req.body;
-let alumno = [nombre,apellido,grado,acudiente,correo,contraseña,cod_trabajo,fecha_nacimiento,id_estudiante,edad];
-
-let nuevoAlumno = `INSERT INTO estudiante(nombre,apellido,grado,acudiente,correo,contraseña,cod_trabajo,fecha_nacimiento,id_estudiante,edad)
-                  VALUES('${nombre}', '${apellido}','${grado}', '${acudiente}','${correo}', '${contraseña}', '${cod_trabajo}, '${fecha_nacimiento}, '${id_estudiante}, '${edad})`;
-mysqlConnection.query(nuevoAlumno, alumno, (err, results, fields) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  res.json({ message:`Alumno matriculado`, })
-  app.get('/',(req,res)=>{
-    res.send('si dio');
-  });
-  });
-});  
-
-router.put('/estudiante/:id_estudiante', (req, res) => {
-  const {contraseña} = req.body;
-  const { id_estudiante, nombre } = req.params;
-  mysqlConnection.query(`UPDATE estudiante SET estudiante.estudiante = '${contraseña}', WHERE id_estudiante = '${id_estudiante}' nombre = '${nombre}'`, 
-  [contraseña,id_estudiante], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Contraseña actualizada'});
+    if (!err) {
+      res.json(rows);
     } else {
       console.log(err);
     }
   });
+});
+
+router.post('/nuevoEstudiante/', (req, res) => {
+
+  const { nombre, apellido, grado, acudiente, correo, contraseña, cod_trabajo, fecha_nacimiento, id_estudiante, edad } = req.body;
+  let alumno = [nombre, apellido, grado, acudiente, correo, contraseña, cod_trabajo, fecha_nacimiento, id_estudiante, edad];
+
+  let nuevoAlumno = `INSERT INTO estudiante(nombre,apellido,grado,acudiente,correo,contraseña,cod_trabajo,fecha_nacimiento,id_estudiante,edad)
+                  VALUES(?,?,?,?,?,?,?,?,?,?)`;
+  mysqlConnection.query(nuevoAlumno, alumno, (err, results, fields) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json({ message: `Alumno matriculado`, })
+    app.get('/', (req, res) => {
+      res.send('si dio');
+    });
+  });
+});
+
+router.put('/estudiante/:id_estudiante', (req, res) => {
+  const { contraseña } = req.body;
+  const { id_estudiante, nombre } = req.params;
+  mysqlConnection.query(`UPDATE estudiante SET estudiante.estudiante = '${contraseña}', WHERE id_estudiante = '${id_estudiante}' nombre = '${nombre}'`,
+    [contraseña, id_estudiante], (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: 'Contraseña actualizada' });
+      } else {
+        console.log(err);
+      }
+    });
 });
 
 // router.delete('/estudiante/:id', (req, res) => {
