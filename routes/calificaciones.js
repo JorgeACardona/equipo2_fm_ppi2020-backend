5efb16786c2c10a1d0be4f50f5b117c4b24363b4
JorgeACardona <andres20cardona@gmail.com>
2020-11-27 21:36:57 -0500

    123

--- routes/calificaciones.js
+++ routes/calificaciones.js
@@ -14,33 +14,45 @@ router.get('/calificaciones', (req, res) => {
     });
   });
 
-router.post('/nueva-calificacion',(req,res)=>{
+  
+  router.get('/estudiantes/:id_calificacion', (req, res) => {
+    const { id_calificacion } = req.params;
+ mysqlConnection.query('SELECT * FROM calificaciones WHERE id_calificacion = ?',[id_calificacion], (err, rows, fields) => {
+     if (!err) {
+       res.json(rows[0]);
+     } else {
+       console.log(err);
+     }
+   });
+ });
 
-const {id_calificacion,materia,periodo,notas} = req.body;
-let calificacion = [id_calificacion,materia,periodo,notas];
+// router.post('/nueva-calificacion',(req,res)=>{
 
-let nuevaCalificacion = `INSERT INTO calificaciones(id_calificacion,materia,periodo,notas)
-                  VALUES(?,?,?,?,?,?,?)`;
-mysqlConnection.query(nuevaCalificacion, calificacion, (err, results, fields) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  res.json({ message:`calificacion agregada`, })
-  });
-});  
+// const {,materia,periodo,notas} = req.body;
+// let calificacion = [id_calificacion,materia,periodo,notas];
 
-router.put('/calificacion/:id_calificacion', (req, res) => {
-  const {notas} = req.body;
-  const {id_calificacion } = req.params;
-  mysqlConnection.query(`UPDATE calificaciones SET calificaciones.calificaciones = '${notas}', WHERE id_calificacion = '${id_calificacion}' `, 
-  [id_calificacion,materia,periodo,notas], (err, rows, fields) => {
-    if(!err) {
-      res.json({status: 'Calificacion actualizada'});
-    } else {
-      console.log(err);
-    }
-  });
-});
+// let nuevaCalificacion = `INSERT INTO calificaciones(id_calificacion,materia,periodo,notas)
+//                   VALUES(?,?,?,?,?,?,?)`;
+// mysqlConnection.query(nuevaCalificacion, calificacion, (err, results, fields) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+//   res.json({ message:`calificacion agregada`, })
+//   });
+// });  
+
+// router.put('/calificacion/:id_calificacion', (req, res) => {
+//   const {notas} = req.body;
+//   const {id_calificacion } = req.params;
+//   mysqlConnection.query(`UPDATE calificaciones SET calificaciones.calificaciones = '${notas}', WHERE id_calificacion = '${id_calificacion}' `, 
+//   [id_calificacion,materia,periodo,notas], (err, rows, fields) => {
+//     if(!err) {
+//       res.json({status: 'Calificacion actualizada'});
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// });
 
 
 module.exports = router;
